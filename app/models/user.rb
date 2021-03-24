9630ef156ecc9dbfcbd68cd6483cc9a46fa5d9f2
dino-pg <79633396+dino-pg@users.noreply.github.com>
2021-03-24 16:51:55 +0900

    ビューファイル配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true, length: { mininum: 6}
+  validates :encrypted_password, presence: true, length: { minimum: 6}
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :kana_family_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
