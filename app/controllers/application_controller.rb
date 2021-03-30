9cd0d4d319584016818c7df0ccc9d9b87c60320e
dino-pg <79633396+dino-pg@users.noreply.github.com>
2021-03-30 12:20:36 +0900

    itemモデルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday])
-
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
   # def index
   #   @users = User.all
   # end
+
+  def new
+   @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def  item_params
+    params.require(:item).permit(:nickname, :image, :text)
+  end
 end
