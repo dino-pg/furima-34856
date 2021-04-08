1db88e754303d246319930a3a03f1c32abcc1364
dino-pg <79633396+dino-pg@users.noreply.github.com>
2021-04-08 18:06:53 +0900

    pay.jpのjemファイルを導入

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -11,18 +11,23 @@ class BuysController < ApplicationController
   end
 
   def create
-    @buy_shipping_address = BuyShippingAddress.new(buy_params)
+    @buy_shipping_address = BuyShippingAddress.new(shippingaddresses_params)
+    @item = Item.find(params[:item_id])
     if @buy_shipping_address.valid?
       @buy_shipping_address.save
-      redirect_to root_path
+    redirect_to root_path
     else
-      render 'new'
+      render :index
     end
   end
 
   private
 
   def buy_params
-    params.require(:buy).permit(:user, :item)
+    params.require(:buy_shipping_address).permit(:user_id, :item_id).merge(token: params[:token])
+  end
+
+  def shippingaddresses_params
+    params.require(:buy_shipping_address).permit(:postal_code, :prefectures_id, :municipality, :address, :phone_number).merge(item_id: @item, user_id: current_user.id)
   end
 end
--- app/models/buy_shipping_address.rb
+++ app/models/buy_shipping_address.rb
@@ -1,6 +1,6 @@
 class BuyShippingAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code, :prefectures_id, :municipality, :address, :phone_number, :building_name, :buy
+  attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :municipality, :address, :phone_number, :building_name, :buy
 
   with_options presence: true do
     validates :postal_code
@@ -8,13 +8,13 @@ class BuyShippingAddress
     validates :municipality
     validates :address
     validates :phone_number
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
     end
     validate :building_name
 
     def save
-      Buy.create(user: user, item: item)
-      ShippingAddress.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, phone_number: phone_number)
+      buy =  Buys.create(user_id: user_id, item_id: item_id)
+      ShippingAddresses.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, phone_number: phone_number)
     end
 end
